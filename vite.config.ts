a198252270641cf48dd961f4b4ba854da573b17f
chenhaichao <chenhaichao@4399.com>
2026-04-21 11:07:38 +0800

    chore: change default dev port to 8800

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import path from 'path'
 export default defineConfig({
   plugins: [react()],
   base: './',
+  server: {
+    port: 8800,
+  },
   build: {
     outDir: 'dist',
     emptyOutDir: true,
